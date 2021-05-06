3c3fc81cecb0570650001947efe088c60dc05124
Nimish Vithalani <nimish.v@somaiya.edu>
2021-05-06 18:24:52 +0530

    docs by id

--- client/src/TextEditor.js
+++ client/src/TextEditor.js
@@ -1,7 +1,8 @@
-import { useCallback, useEffect } from 'react'
+import { useCallback, useEffect, useState } from 'react'
 import Quill from 'quill'
 import 'quill/dist/quill.snow.css'
 import { io } from 'socket.io-client'
+import { useParams } from 'react-router-dom'
 
 const TOOLBAR_OPTIONS = [
     [{ header: [1, 2, 3, 4, 5, 6, false] }],
@@ -16,21 +17,76 @@ const TOOLBAR_OPTIONS = [
   ]
 
 export default function TextEditor() {
+    const [socket, setsocket] = useState()
+    const [quill, setQuill] = useState()
+    const { id: documentId } = useParams()
 
     useEffect(() => {
-        const socket = io("http://localhost:3001")
+        const s = io("http://localhost:3001")
+        setsocket(s)
 
         return () => {
-            socket.disconnect()
+            s.disconnect()
         }
     }, [])
+
+    useEffect(() => {
+        if(quill == null || socket == null) return
+        const handler = delta => {
+            quill.updateContents(delta)
+        }
+        socket.on('recieve-changes', handler)
+        return () => {
+            socket.off('recieve-changes', handler)
+        }
+    }, [socket, quill])
+
+    useEffect(() => {
+        if(socket == null || quill == null) return
+
+        socket.once('load-document', document => {
+            quill.setContents(document)
+            quill.enable()
+        })
+
+        socket.emit('get-document', documentId)
+
+    }, [socket, quill, documentId])
+
+    useEffect(() => {
+        if(socket == null || quill == null) return
+
+        const interval = setInterval(() => {
+            socket.emit('save-document', quill.getContents())
+        }, 2000)
+
+        return() => {
+            clearInterval(interval)
+        }
+    }, [socket, quill])
+
+    useEffect(() => {
+        if(quill == null || socket == null) return
+        const handler = (delta, oldDelta, source) => {
+            if(source !== 'user') return
+            socket.emit("send-changes", delta)
+        }
+        quill.on('text-change', handler)
+        return () => {
+            quill.off('text-change', handler)
+        }
+    }, [socket, quill])
 
     const wrapperRef = useCallback((wrapper) => {
         if(wrapper == null) return;
         wrapper.innerHtml = '';
         const editor = document.createElement('div');
         wrapper.append(editor);
-        new Quill(editor, {theme: "snow", modules: {toolbar : TOOLBAR_OPTIONS}})
+        const q = new Quill(editor, {theme: "snow", modules: {toolbar : TOOLBAR_OPTIONS},})
+
+        q.disable()
+        q.setText('Wait....')
+        setQuill(q)
     }, []);
     return (
         <div className="container" ref={wrapperRef}></div>
--- server/server.js
+++ server/server.js
@@ -1,3 +1,13 @@
+const mongoose = require('mongoose')
+const Document = require('./document')
+
+mongoose.connect('mongodb://localhost/solid', {
+    useNewUrlParser : true,
+    useUnifiedTopology : true,
+    useFindAndModify : false,
+    useCreateIndex : true
+})
+
 const io = require("socket.io")(3001, {
     cors: { 
         origin :  "http://localhost:3000",
@@ -5,6 +15,30 @@ const io = require("socket.io")(3001, {
     },
 })
 
+const defaultValue = ""
+
 io.on("connection", socket => {
-    console.log("connected")
+
+    socket.on('get-document', async documentId => {
+        const document = await findOrCreatebtId(documentId)
+        socket.join(documentId)
+        socket.emit('load-document', document.data)
+
+        socket.on('send-changes', delta => {
+            socket.broadcast.to(documentId).emit('recieve-changes', delta)
+        })
+
+        socket.on('save-changes', async data => {
+            await Document.findByIdAndUpdate(documentId, {data})
+        })
+    })
 })
+
+async function findOrCreatebtId(id){
+    if(id == null ) return
+
+    const document = await Document.findById(id)
+    if(document) return document
+    return await Document.create({ _id : id, data : defaultValue})
+
+}
