69857a20dfa30ee5dff68432bbe50760717f3ef8
Nimish Vithalani <nimish.v@somaiya.edu>
2021-05-06 16:39:42 +0530

    Routes added

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,26 @@
 import TextEditor from './TextEditor'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from 'react-router-dom'
+
+import { v4 as uuidv4 } from 'uuid'
 
 function App() {
-  return <TextEditor/>
+  return (
+    <Router>
+      <Switch>
+        <Route path='/' exact>
+          <Redirect to={`/documents/${uuidv4()}`}/>
+        </Route>
+        <Route path='/documents/:id' exact>
+          <TextEditor/>
+        </Route>
+      </Switch>
+  </Router>
+  )
 }
 
 export default App;
